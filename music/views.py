d9fedb3375ff72ccc932a8285c459fa97cccb486
priom <deathrazer007@yahoo.com>
2016-05-04 03:57:45 -0300

    project added

---
+++ music/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Album
+
+
+def index(request):
+    all_albums = Album.objects.all()
+    return render(request, 'music/index.html', {'all_albums': all_albums})
+
+
+def detail(request, album_id):
+    album = get_object_or_404(Album, id=album_id)
+    return render(request, 'music/detail.html', {'album': album})
+
