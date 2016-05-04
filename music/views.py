ab4d524e2c25b64c919876d2be83454d0eb6fade
priom <deathrazer007@yahoo.com>
2016-05-04 05:06:00 -0300

    project added

--- music/views.py
+++ music/views.py
@@ -11,3 +11,18 @@ def detail(request, album_id):
     album = get_object_or_404(Album, id=album_id)
     return render(request, 'music/detail.html', {'album': album})
 
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, id=album_id)
+    try:
+        selected_song = album.song_set.get(id=request.POST['song'])
+    except(KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {
+            'album': album,
+            'err_msg': "Not a valid song.",
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
+
