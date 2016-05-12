ceb8d556b4f4abc87afe934c5e764c07b3985a81
priom <deathrazer007@yahoo.com>
2016-05-11 23:37:43 -0300

    project updated

--- music/urls.py
+++ music/urls.py
@@ -5,14 +5,23 @@ from . import views
 
 app_name = 'music'
 
+
 urlpatterns = [
     # /music/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
-    # /music/<album_id>
-    url(r'^(?P<album_id>[0-9]+)$', views.detail, name="detail"),
+    # register
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
 
-    # /music/<album_id>/favorite/
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name="favorite"),
+    # /music/<album_id> .... pk == id
+    url(r'^(?P<pk>[0-9]+)$', views.DetailView.as_view(), name='detail'),
 
+    # /music/album/add
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
+    # /music/album/pk:edit
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+
+    # /music/album/pk:delete
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
 ]
